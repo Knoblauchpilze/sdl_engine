0752f18433081ea8de1226a38a9136ffb4ffe062
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2019-06-30 12:42:43 +0200

    Implemented merge strategy in 'ResizeEvent' and made it dependent on the fact that both events should be consecutive, i.e. the new size of the first event should correspond to the old size of the second event

--- src/events/ResizeEvent.cc
+++ src/events/ResizeEvent.cc
@@ -11,6 +11,39 @@ namespace sdl {
         engine.populateEvent(*this);
       }
 
+      bool
+      ResizeEvent::mergePrivate(const Event& other) noexcept {
+        // Use the base handler to determine whether the input event can be merged with
+        // this event.
+        bool canMerge = Event::mergePrivate(other);
+
+        if (!canMerge) {
+          // Return early.
+          return canMerge;
+        }
+
+        // Convert the input `other` event to usable data type.
+        const ResizeEvent& usable = dynamic_cast<const ResizeEvent&>(other);
+
+        // Equalize internal fields: we only want to equalize the new size and
+        // not the old size. Indeed when we want to merge two resize event and
+        // this operation is permitted by the base `Event` class, it means that
+        // the `other` event is more recent than this one. Normally this should
+        // mean that the `old size` of the `other` event corresponds to the `new
+        // size` of `this` event. If this is not the case this is a problem.
+        if (m_new != usable.m_old) {
+          log(
+            std::string("Merging resize event but sizes don't match (expected ") + m_new.toString() + " got " + usable.m_old.toString(),
+            utils::Level::Warning
+          );
+        }
+
+        m_new = usable.m_new;
+
+        // Return the base handler's prescriptions.
+        return canMerge;
+      }
+
     }
   }
 }
