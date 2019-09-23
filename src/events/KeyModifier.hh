43edcac5d13856916837a430f7ef1b46ddfbbf6f
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2019-09-23 22:25:02 +0200

    Fixed typos

--- src/events/KeyModifier.hh
+++ src/events/KeyModifier.hh
@@ -29,7 +29,7 @@ namespace sdl {
         /**
          * @brief - Retrieves a human readable name from the input modifiers.
          * @param modifiers - a flag containing several modifiers for which a
-         *                    a name should be provided.
+         *                    name should be provided.
          * @return - a string describing the names of the modifiers registered
          *           in the input argument.
          */
@@ -102,7 +102,7 @@ namespace sdl {
            *          modifier mode when registereing it through the `addNamedBit`
            *          interface. If no such information is available an error is
            *          raised.
-           * @param mode - the mode for which the bit index should be retrieved
+           * @param mode - the mode for which the bit index should be retrieved.
            * 
            * @return - the index of the bit in the base class as returned by the
            *           `addNamedBit` method.
