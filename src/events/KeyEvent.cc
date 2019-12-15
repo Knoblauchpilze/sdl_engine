d2ae45918424171accde63fff2530b87dbb247f9
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2019-12-15 17:39:34 +0100

    CHanged verbosity of 'KeyEvent' class

--- src/events/KeyEvent.cc
+++ src/events/KeyEvent.cc
@@ -62,7 +62,8 @@ namespace sdl {
           "key " + std::to_string(m_key.keysym.sym) + " " +
           "(scan: " + std::to_string(m_key.keysym.scancode) + ", name: \"" + SDL_GetKeyName(m_key.keysym.sym) + "\") " +
           "converted to " + std::to_string(static_cast<int>(m_raw)) + " "
-          " (mode: " + keyboard::toString(getKeyboardLayout()) + ")"
+          " (mode: " + keyboard::toString(getKeyboardLayout()) + ")",
+          utils::Level::Verbose
         );
 
         setType(type);
